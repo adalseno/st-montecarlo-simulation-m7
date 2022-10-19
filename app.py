9fa1cb8283c3400f7d00277a74c30fc35be6f62e
Andrea Dalseno <adalseno@yahoo.com>
2022-10-19 18:22:24 +0200

    Updated app.py
    Refactored code to use numba

--- app.py
+++ app.py
@@ -1,53 +1,69 @@
-import streamlit as st
 import numpy as np
-import numexpr as ne
+from numba import jit
 import pandas as pd
+import streamlit as st
 
 
 # Helper function that creates distribution and statistics
-def Hist(trials:int)-> tuple[np.array, pd.DataFrame]:
-   
-    # Vectorized version, much faster (Annual interest from 40% to 70% random normal)
-    distribution = (1+np.random.normal(0.4, 0.7, (12,trials))/12)
-    interest = np.array([range(12,0,-1)]).T
-    #res = (10*((distribution)**interest)).sum(axis=0)
-    res = ne.evaluate("sum(10*(distribution**interest),axis=0)") # use numexpr engine
-   
-    # Add statistics
-    avg = np.mean(res)  # Mean
-    sigma = np.std(res)  # Std
-    lowb = (avg - 1.96 * sigma / np.sqrt(trials))  # Low Bound
-    upb = (avg + 1.96 * sigma / np.sqrt(trials))  # Upper Bound
-    width = (upb - lowb)
+@jit(
+    nopython=True, parallel=True
+)  # Set "nopython" mode for best performance, equivalent to @njit
+def Hist(trials: int) -> np.array:
 
-    # Create DataFrame with statistics 
-    stat_df = pd.DataFrame({"Trials": f"{trials:,}", "Average": f"$ {avg:.2f}", "St. Dev": f"$ {sigma:.2f}", 
-                    "Upper Bound": f"$ {upb:.2f}", "Low Bound": f"$ {lowb:.2f}", "Width": f"$ {width:.2f}"}, 
-                    index=["Monte Carlo Simulation"])
-    return res, stat_df
+    # Vectorized version, much faster (Annual interest from 40% to 70% random normal)
+    distribution = 1 + np.random.normal(0.4, 0.7, (12, trials)) / 12
+    interest = np.arange(12, 0, -1).reshape(-1, 1)
+    res = (10 * np.power(distribution, interest)).sum(axis=0)
+
+    return res
 
 
 # Title and subtitle
-st.title('Monte Carlo simulation')
-st.subheader("Cumulative Account Balance with a Monthly $10 Dollar Investment and Variable Annual Interest")
+st.title("Monte Carlo simulation")
+st.subheader(
+    "Cumulative Account Balance with a Monthly $10 Dollar Investment and Variable Annual Interest"
+)
 
-    
+
 # Select number of trials from listbox
-trial_range = map(lambda x: int(np.power(10,x)), range(2,8)) # from 100 to 10_000_000
+trial_range = map(lambda x: int(np.power(10, x)), range(2, 8))  # from 100 to 10_000_000
 
 # I used a comma to seprate thousands for better readability
-trials = st.selectbox('Select number of trials', trial_range, format_func=lambda x: f"{x:,}")
+trials = st.selectbox(
+    "Select number of trials", trial_range, format_func=lambda x: f"{x:,}"
+)
 
 # Get data
-data_load_state = st.text('Computing data...')
-data, stat_df = Hist(trials)
+data_load_state = st.text("Computing data...")
+data = Hist(trials)
 data_load_state.text("Done!")
+
+# Add statistics
+avg = np.mean(data)  # Mean
+sigma = np.std(data)  # Std
+lowb = avg - 1.96 * sigma / np.sqrt(trials)  # Low Bound
+upb = avg + 1.96 * sigma / np.sqrt(trials)  # Upper Bound
+width = upb - lowb
+
+# Create DataFrame with statistics
+stat_df = pd.DataFrame(
+    {
+        "Trials": f"{trials:,}",
+        "Average": f"$ {avg:.2f}",
+        "St. Dev": f"$ {sigma:.2f}",
+        "Upper Bound": f"$ {upb:.2f}",
+        "Low Bound": f"$ {lowb:.2f}",
+        "Width": f"$ {width:.2f}",
+    },
+    index=["Monte Carlo Simulation"],
+)
+
 
 # Get bounds
 bound_min = int(data.min().round(0))
 bound_max = int(data.max().round(0))
 
-# Get data for histogram 
+# Get data for histogram
 y = np.histogram(data, bins=24, density=True)[0]
 x = np.linspace(bound_min, bound_max, len(y), dtype=int)
 
